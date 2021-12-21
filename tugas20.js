2b7d3c1a9360c6ebe8e4b7cda345484973c91f7e
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-21 22:34:20 +0700

    Pengumpulan Tugas 20 JS - NIOMIC

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+console.log("MATCH\n==================================");
+
+const teks =
+  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+console.log(teks.match("are"));
+console.log(teks.match("aku"));
+console.log(teks.match("words"));
